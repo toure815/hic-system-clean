2c2e0a1a021d1c52227afcd7e937e3e07ab42703
Leap <git@leap.new>
2025-09-01 21:05:51 +0000

    Fix Supabase URL error by using config

--- frontend/utils/supabase.ts
+++ frontend/utils/supabase.ts
@@ -1,6 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+import { supabaseUrl, supabaseAnonKey } from "../config";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
