4ab014665a922aa725d932243f5200c924a71e0d
toure815 <ecrofmedia@gmail.com>
2025-10-06 17:53:00 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,14 +6,27 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname),
+      '@': path.resolve(__dirname, './'),
       '~backend/client': path.resolve(__dirname, './client'),
       '~backend': path.resolve(__dirname, '../backend'),
     },
   },
-  plugins: [tailwindcss(), react()],
-  mode: "development",
+  plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     minify: false,
-  }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['hic-system.onrender.com'], // 👈 this line fixes your 403
+  },
 })
